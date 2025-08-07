3f34fe09edb82ab0f698c3dd442134617d564849
Xin Zhe Khooi <khooixz@comp.nus.edu.sg>
2025-08-07 12:25:01 +0800

    cu_updates: update image list

--- cu_updates/scripts/prepull_images.sh
+++ cu_updates/scripts/prepull_images.sh
@@ -5,5 +5,8 @@ set -x
 sudo ctr image pull docker.io/khooi8913/f1ap_proxy:swapran
 sudo ctr image pull docker.io/oaisoftwarealliance/oai-gnb:2025.w29
 sudo ctr image pull docker.io/oaisoftwarealliance/oai-gnb:2025.w30
+sudo ctr image pull docker.io/oaisoftwarealliance/oai-gnb:2025.w31
 sudo ctr image pull docker.io/oaisoftwarealliance/oai-gnb-fhi72:2025.w29
 sudo ctr image pull docker.io/oaisoftwarealliance/oai-gnb-fhi72:2025.w30
+sudo ctr image pull docker.io/oaisoftwarealliance/oai-gnb-fhi72:2025.w31
+sudo ctr image pull docker.io/oaisoftwarealliance/oai-tcpdump-init:alpine-3.20
