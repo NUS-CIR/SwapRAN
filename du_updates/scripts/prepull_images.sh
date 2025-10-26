6bcd37d48f34e6db0a168f73058648ac266ea859
Xin Zhe Khooi <khooixz@comp.nus.edu.sg>
2025-10-26 16:22:31 +0800

    du_updates: revise pull image script

--- du_updates/scripts/prepull_images.sh
+++ du_updates/scripts/prepull_images.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-sudo ctr image pull docker.io/oaisoftwarealliance/oai-gnb-fhi72:2025.w14
-sudo ctr image pull docker.io/oaisoftwarealliance/oai-gnb-fhi72:2025.w18
-sudo ctr image pull docker.io/khooi8913/oai-gnb-fhi72:2025.w14
-sudo ctr image pull docker.io/khooi8913/oai-gnb-fhi72:2025.w18
+sudo ctr image pull docker.io/oaisoftwarealliance/oai-gnb-fhi72:2025.w29
+sudo ctr image pull docker.io/oaisoftwarealliance/oai-gnb-fhi72:2025.w30
+sudo ctr image pull docker.io/oaisoftwarealliance/oai-gnb-fhi72:2025.w31
+sudo ctr image pull docker.io/khooi8913/oai-gnb-fhi72:2025.w29
+sudo ctr image pull docker.io/khooi8913/oai-gnb-fhi72:2025.w30
+sudo ctr image pull docker.io/khooi8913/oai-gnb-fhi72:2025.w31
+sudo ctr image pull docker.io/oaisoftwarealliance/oai-tcpdump-init:alpine-3.20
